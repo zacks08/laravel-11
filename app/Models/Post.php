922f1ae098896b85f60cab0020e02f6a0d5376b6
isaac <isaacchagas011@gmail.com>
2025-10-20 18:03:02 -0300

    feat: enable soft delete on model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,19 +1,20 @@
 <?php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use SoftDeletes;
     protected $fillable = ['title', 'body', 'user_id'];
-
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
-    
+
     public function comments()
     {
         return $this->hasMany(Comment::class)->latest();
